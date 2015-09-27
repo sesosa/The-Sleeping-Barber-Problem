c9a893ffe41f746e49befa0ea3d5df75f87947f8
Savannah Sosa <sesosa@Savannahs-MacBook-Pro.local>
2015-09-26 23:06:59 -0400

    Initial Commit

---
+++ barberTest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  barberTest
+//
+//  Created by Savannah Sosa on 9/26/15.
+//  Copyright (c) 2015 Savannah Sosa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
